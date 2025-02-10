fe8430703f3afe30c0c9ad36aa75111e48c1bebc
abdi <abdiyaya74@gmail.com>
2025-02-10 01:17:45 -1000

    condition

--- alert.js
+++ alert.js
@@ -1,5 +1,11 @@
-let isboss = prompt("what is your name?", "");
-let n = 2;
-n += 5;
-n *= 2;
-alert(n);
+let year = prompt(
+  "in which year was EcmaScript-2015 specication Published",
+  ""
+);
+if (year < 2015) {
+  alert("too early...");
+} else if (year > 2015) {
+  alert("Too late..");
+} else {
+  alert("Exactly..");
+}
