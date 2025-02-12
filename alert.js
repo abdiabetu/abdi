3961db5a6de83239e8e035f11c4c23cd8edfb4f1
abdi <abdiyaya74@gmail.com>
2025-02-12 08:45:59 -1000

    commit

--- alert.js
+++ alert.js
@@ -1,11 +1,2 @@
-let year = prompt(
-  "in which year was EcmaScript-2015 specication Published",
-  ""
-);
-if (year < 2015) {
-  alert("too early...");
-} else if (year > 2015) {
-  alert("Too late..");
-} else {
-  alert("Exactly..");
-}
+let i = 5;
+while (i) alert(i--);
